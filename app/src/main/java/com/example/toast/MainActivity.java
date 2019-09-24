7e3397027c6dc3b35209a93d211e095790c06eb9
David Usón <xldaviduson@gmail.com>
2019-09-24 17:25:40 +0200

    Practica acabada

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Context con = getApplicationContext();
+                CharSequence text = "Holita vecinito";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(con, text, duration);
+                toast.show();
+            }
+        });
     }
 }
